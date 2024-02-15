20e82c6c47fd205a11eaeb7637cbed77e26541d0
Rishav Chandel <82945614+Rgsis-commits@users.noreply.github.com>
2024-02-16 03:08:50 +0530

    Update Calculator.java

--- src/main/java/com/example/calculator_spe/Calculator.java
+++ src/main/java/com/example/calculator_spe/Calculator.java
@@ -10,7 +10,7 @@ public class Calculator{
     public int factorial(@PathVariable int number) {
         if (number == 1)
             return 1;
-        if (number == 0)
+        else if (number == 0)
             return -1;
         else
             return (number * factorial(number - 1));
