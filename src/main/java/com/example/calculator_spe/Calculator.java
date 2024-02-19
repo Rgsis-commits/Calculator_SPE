c0cdfb382c579b1fe4ba319fc23abcc7894ad2ba
Rishav Chandel <rishavchandel456@gmail.com>
2024-02-19 19:58:28 +0530

    localhost used

--- src/main/java/com/example/calculator_spe/Calculator.java
+++ src/main/java/com/example/calculator_spe/Calculator.java
@@ -2,7 +2,7 @@ package com.example.calculator_spe;
 
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = {"http://localhost:3000"})
+@CrossOrigin(origins = {"http://localhost:3000/"})
 @RestController
 public class Calculator{
 
